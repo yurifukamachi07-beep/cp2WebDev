155f35048251c5ab2ece55ef920e979eb1225e67
yurifukamachi07-beep <yurifukamachi07@gmail.com>
2025-11-01 02:09:57 -0300

    Vigenere / readme.md

--- Cifras/vigenere.js
+++ Cifras/vigenere.js
@@ -1,7 +1,41 @@
-function cifrarVigenere(mesagem, chave, modo = codificar) {
-    let resultado_c = '';
-}
+function cifrarVigenere(mensagem, palavraChave, modo = 'codificar') {
+  let resultado = '';
+  let chaveIndex = 0;
 
-function cifrarVigenere(mesagem, chave, modo = decodificar) {
-    let resultado_d = ' ';
+  for (let i = 0; i < mensagem.length; i++) {
+    let char = mensagem[i];
+    let codigo = mensagem.charCodeAt(i);
+    let letraBase;
+    let novaLetra;
+    
+    if (codigo >= 65 && codigo <= 90) {
+      letraBase = 65; // base 'A'
+    }
+    else if (codigo >= 97 && codigo <= 122) {
+      letraBase = 97; // base 'a'
+    }
+    else {
+      resultado += char;
+      continue;
+    }
+
+    let letraMensagem = codigo - letraBase;
+    let codigoChave = palavraChave.charCodeAt(chaveIndex % palavraChave.length);
+    if (codigoChave >= 65 && codigoChave <= 90) {
+      codigoChave += 32;
+    }
+
+    let letraChave = codigoChave - 97;
+    let deslocamento;
+    if (modo === 'codificar') {
+      deslocamento = (letraMensagem + letraChave) % 26;
+    } else {
+      deslocamento = (letraMensagem - letraChave + 26) % 26;
+    }
+
+    novaLetra = String.fromCharCode(letraBase + deslocamento);
+    resultado += novaLetra;
+    chaveIndex++;
+  }
+  return resultado;
 }
---
+++ readme.md
@@ -0,0 +1,29 @@
+# Projeto de Criptografia Clássica em JavaScript
+
+Integrantes do Grupo
+- Kenzo Hiro Costa Mimura - RM 568158  
+- Yuri Aroste Fukamachi - RM 567314  
+- Michel Lima Benchimol - RM 567345  
+
+Descrição do Projeto
+Este projeto tem como objetivo implementar quatro das cifras mais importantes da história da criptografia utilizando apenas JavaScript Vanilla.
+- Funções JavaScript  
+- for
+- Arrays  
+- Manipulação de strings e códigos ASCII  
+- %  
+
+As cifras implementadas preservam o **case** (maiúsculas/minúsculas) e mantêm **espaços, números e pontuações** inalterados.
+
+Cifras Implementadas
+1. **Cifra de César**  
+   Uma cifra de substituição simples que desloca cada letra do alfabeto por um número fixo de posições.
+
+2. **Cifra de Vigenère**  
+   Uma cifra polialfabética que utiliza uma palavra-chave para determinar o deslocamento de cada letra.
+
+3. **Cifra Atbash**  
+   Uma cifra monoalfabética que substitui cada letra pela sua correspondente invertida no alfabeto (A ↔ Z, B ↔ Y, etc.).
+
+4. **Cifra de ROT13**  
+   Uma cifra de substituição que desloca cada letra 13 posições no alfabeto, sendo uma variante simples da Cifra de César.
